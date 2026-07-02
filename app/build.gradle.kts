6f79360bbeb0f6fbdd4d6123798441055e2fa00c
Manish <manish.agrawal.np@gmail.com>
2026-07-02 22:29:28 +0530

    dependencies updated

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,12 +8,12 @@ plugins {
 
 android {
   namespace = "com.example"
-  compileSdk { version = release(36) { minorApiLevel = 1 } }
+  compileSdk = 37
 
   defaultConfig {
     applicationId = "com.aistudio.wordscapes.yrmqxw"
     minSdk = 24
-    targetSdk = 36
+    targetSdk = 37
     versionCode = 1
     versionName = "1.0"
 
